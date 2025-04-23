17d760b37925f75ae78c914dd57eb0fda7fdc81c
ansgrb <ansgrb@gmail.com>
2025-04-23 18:43:36 +0200

    Handle explicit response for specific websocket messages.
    Added a case to respond with "Fuck you too!" when receiving "fuck you" (case-insensitive). This complements the existing logic and provides a more interactive user experience.

--- src/main/kotlin/Sockets.kt
+++ src/main/kotlin/Sockets.kt
@@ -21,8 +21,11 @@ fun Application.configureSockets() {
 		webSocket("/ws") { // websocketSession
 			for (frame in incoming) {
 				if (frame is Frame.Text) {
-					val text = frame.readText()
+					val text = frame.readText()             	
 					outgoing.send(Frame.Text("YOU SAID: $text"))
+					if (text.equals("fuck you", ignoreCase = true)) {
+						outgoing.send(Frame.Text("Fuck you too!"))
+					}
 					if (text.equals("bye", ignoreCase = true)) {
 						close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
 					}
